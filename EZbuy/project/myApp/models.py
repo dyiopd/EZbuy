1bf37e747568d8d31716f57cd49b079ae3c48508
dyiopd <33553619+dyiopd@users.noreply.github.com>
2018-07-07 20:29:13 -0500

    Add files via upload

--- EZbuy/project/myApp/models.py
+++ EZbuy/project/myApp/models.py
@@ -1,8 +1,34 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+
 # Create your models here.
 
-class Users(models.Model):
-    UserID = models.CharField(max_length=20)
-    # models.IntegerField()
-    # models.BooleanField()
+# class Users(models.Model):
+#     username = models.IntegerField(unique=True)
+#     userpwd = models.CharField(max_length=50)
+#     useremail = models.CharField(max_length=100)
+#     usertel = models.IntegerField(null=True,blank=True)
+#
+#     class Meta:
+#         abstract = True
+#
+#     def __str__(self):
+#         return self.username
+#
+#
+# class Sellers(Users):
+#     postid = models.IntegerField()
+#
+#
+# class Customers(Users):
+#     pass
+#
+#
+class Products(models.Model):
+    productName = models.CharField(max_length=40)
+    productInformation = models.CharField(max_length=100)
+    productCategory = models.IntegerField()
+    productImage = models.ImageField(upload_to='img')
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
--- EZbuy/project/myApp/views.py
+++ EZbuy/project/myApp/views.py
@@ -1,8 +1,98 @@
+from django.contrib import auth
 from django.shortcuts import render
+import django.contrib.auth
+from .models import *
+from django.contrib.auth.models import User
 
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html/')
+    username = request.session.get('username', "sign in")
+    return render(request, 'index.html/', {'username': username})
+
+
+def signin(request):
+    return render(request, 'signin.html/')
+
+
+def signup(request):
+    return render(request, 'signup.html/')
+
+
+def mobiles(request):
+    return render(request, "mobiles.html/")
+
+
+def electronics(request):
+    return render(request, 'electronics-appliances.html/')
+
+
+# redirect
+from django.http import HttpResponseRedirect
+
+
+def redirectSignup(request):
+    return HttpResponseRedirect('/signup')
+
+
+def redirectSignin(request):
+    return HttpResponseRedirect('/signin')
+
+
+def redirectIndex(request):
+    return HttpResponseRedirect('/index')
+
+
+def back(request):
+    username = request.POST.get('Name')
+    request.session['username'] = username
+    return HttpResponseRedirect('/index')
+
+
+def register(request):
+    username = request.POST.get('Name')
+    # request.session['username'] = username
+    password = request.POST.get('Password')
+    email = request.POST.get('Email')
+    registerAdd = User.objects.create_user(username=username, password=password, email=email)
+
+    if not registerAdd:  # didn't register
+        return render(request, 'kids.html/')
+    else:  # register successfully
+        request.session['username'] = username
+        return HttpResponseRedirect('/index')
+
+
+def login(request):
+    username = request.POST.get('Name')
+    password = request.POST.get('Password')
+    re = auth.authenticate(username=username, password=password)
+
+    if re is not None:  # if username and password matches the data in database
+        auth.login(request, re)  # login successfully
+        request.session['username'] = username
+        return HttpResponseRedirect('/index')  # jump to homepage
+    else:  # there is no such data in database
+        return render(request, 'signin.html', {'login_error': 'username or password error'})  # display error message
+    return render(request, 'signin.html')
+
+
+# def main(request):
+#     username = request.session.get('name', "log in")
+#     return render(request, 'main.html', {'username': username})
+#
+#
+# def login(request):
+#     return render(request, 'login.html')
+#
+#
+# def showmain(request):
+#     username = request.POST.get('username')
+#     request.session['name'] = username
+#     return HttpResponseRedirect('/main')
+
+
+def logout(request):
+    django.contrib.auth.logout(request),
+    return HttpResponseRedirect('/index')

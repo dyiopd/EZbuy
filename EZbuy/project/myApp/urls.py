b7c5bca5a4b97547bd4e30389f5a4a8428bc4acf
dyiopd <33553619+dyiopd@users.noreply.github.com>
2018-07-07 20:28:24 -0500

    Add files via upload

--- EZbuy/project/myApp/urls.py
+++ EZbuy/project/myApp/urls.py
@@ -1,8 +1,62 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [
+urlpatterns = (
 
-    url(r'^$', views.index)
+    # url to index page
+    url(r'^index/$', views.index),
 
-]
+    url(r'^login/index/$', views.redirectIndex),
+
+    # url to sign up page
+    url(r'^signup/$', views.signup),
+
+    # url to sign in page
+    url(r'^signin/$', views.signin),
+
+    # url to mobile page
+    url(r'^index/mobiles/$', views.mobiles),
+
+    # url to electronics page
+    url(r'^index/electronics/$', views.electronics),
+
+    # redirect the url to sign up page
+    url(r'^login/signup/$', views.redirectSignup),
+
+    url(r'^signin/signup/$', views.redirectSignup),
+
+    # redirect the url to sign in page
+    url(r'^signup/signin/$', views.redirectSignin),
+
+    # redirect the url to sign in page
+    url(r'^signin/signin/$', views.redirectSignin),
+
+    # redirect the url to index page
+    url(r'^signin/index/$', views.redirectIndex),
+
+    url(r'^index/index/$', views.redirectIndex),
+
+    # redirect the url to index page
+    url(r'^signup/index/$', views.redirectIndex),
+
+    url(r'^back/$', views.back),
+
+    url(r'^index/signin/$', views.redirectSignin),
+
+    # url(r'^main/$', views.main),
+    #
+    # url(r'^main/login/$', views.login),
+    #
+    # url(r'^showmain/$', views.showmain),
+
+    url(r'^index/quit/$', views.logout),
+
+    url(r'^login/quit/$', views.logout),
+
+    url(r'^register/quit/$', views.logout),
+
+    url(r'^register/$', views.register),
+
+    url(r'^login/$', views.login),
+
+)

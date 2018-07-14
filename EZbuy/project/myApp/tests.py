07ce57390a6b1004e82a38d33c5226a3d227322b
dyiopd <343005092@qq.com>
2018-07-14 16:59:28 -0500

    Add unit test scripts
    make unit test scripts that test the user sign in and sign up function, and the html web page return status

--- EZbuy/project/myApp/tests.py
+++ EZbuy/project/myApp/tests.py
@@ -1,3 +1,61 @@
-from django.test import TestCase
+import os
+import unittest
 
+from django.test import TestCase
 # Create your tests here.
+import django
+
+
+def check_django_environment(default_settings):
+    # Environment setup for Django project files:
+    os.sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    if not os.environ.get('DJANGO_SETTINGS_MODULE'):
+        # Don't override settings if it is specified.
+        os.environ['DJANGO_SETTINGS_MODULE'] = default_settings
+        from django.conf import settings
+
+        return getattr(settings, 'DEBUG', None)
+
+
+check_django_environment('project.settings')
+
+django.setup()
+from django.contrib.auth.models import User
+from django.test import Client
+
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+        User.objects.create_user(username='test1', email="123@163.com", password="123")
+
+    def testUser(self):
+        u = User.objects.get(username='test1')
+        self.assertEqual(u.username, 'test1')
+        self.assertEqual(u.email, '123@163.com')
+
+    def testIndex(self):
+        c = Client()
+        response = c.post('/index/')
+        self.assertEqual(response.status_code, 200)
+
+    def testSignin(self):
+        c = Client()
+        c.login(username='test', password='123')
+        response = c.post('/signin/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(self, c.login(username='test', password='123'))
+
+    def testSignup(self):
+        c = Client()
+        response = c.post('/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def testMobiles(self):
+        c = Client()
+        response = c.post('/mobiles/0/')
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()

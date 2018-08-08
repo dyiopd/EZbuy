8a4b9efe81db937e348250fbdfb2875caa8cc281
Chenxi <33553619+dyiopd@users.noreply.github.com>
2018-08-08 15:20:37 -0500

    update test file

--- EZbuy/project/myApp/tests.py
+++ EZbuy/project/myApp/tests.py
@@ -22,17 +22,45 @@ check_django_environment('project.settings')
 django.setup()
 from django.contrib.auth.models import User
 from django.test import Client
+from django.http import HttpRequest
+
+from myApp.models import Products, Comments
+from myApp.views import mobiles
+
+from django.contrib.sessions.models import Session
+from django.contrib.sessions.backends.db import SessionStore
 
 
 class UserTestCase(TestCase):
 
     def setUp(self):
-        User.objects.create_user(username='test1', email="123@163.com", password="123")
+        u = User.objects.create_user(username='test1', email="123@163.com", password="123")
+        Products.objects.create(productName='aa', productPrice=12, productInformation='aa', productCategory=1,
+                                productImage='img/IMG_0400_hLhRP8Q.JPG', isDelete=False, buyer=u)
+        Products.objects.create(productName='ele', productPrice=12, productInformation='ele', productCategory=2,
+                                productImage='img/IMG_0400_hLhRP8Q.JPG', isDelete=False, buyer=u)
+        Products.objects.create(productName='bike', productPrice=12, productInformation='bike', productCategory=3,
+                                productImage='img/IMG_0400_hLhRP8Q.JPG', isDelete=False, buyer=u)
+        Products.objects.create(productName='book', productPrice=12, productInformation='book', productCategory=4,
+                                productImage='img/IMG_0400_hLhRP8Q.JPG', isDelete=False, buyer=u)
+        Comments.objects.create(description='Nice', owner=u)
 
     def testUser(self):
         u = User.objects.get(username='test1')
         self.assertEqual(u.username, 'test1')
         self.assertEqual(u.email, '123@163.com')
+
+    def testProducts(self):
+        p = Products.objects.get(productName='aa')
+        self.assertEqual(p.productName, 'aa')
+        self.assertEqual(p.productPrice, 12)
+        self.assertEqual(p.productInformation, 'aa')
+        self.assertEqual(p.productCategory, 1)
+        self.assertEqual(p.productImage, 'img/IMG_0400_hLhRP8Q.JPG')
+
+    def testComments(self):
+        c = Comments.objects.get(description='Nice')
+        self.assertEqual(c.description, 'Nice')
 
     def testIndex(self):
         c = Client()
@@ -53,8 +81,35 @@ class UserTestCase(TestCase):
 
     def testMobiles(self):
         c = Client()
-        response = c.post('/mobiles/0/')
+        response = c.post('/mobiles/1/0/')
+        productList = Products.objects.filter(productCategory='1')
         self.assertEqual(response.status_code, 200)
+        for product in productList:
+            self.assertEqual(product.productCategory, 1)
+
+    def testElectronics(self):
+        c = Client()
+        response = c.post('/electronics/1/0/')
+        productList = Products.objects.filter(productCategory='2')
+        self.assertEqual(response.status_code, 200)
+        for product in productList:
+            self.assertEqual(product.productCategory, 2)
+
+    def testBikes(self):
+        c = Client()
+        response = c.post('/bikes/1/0/')
+        productList = Products.objects.filter(productCategory='3')
+        self.assertEqual(response.status_code, 200)
+        for product in productList:
+            self.assertEqual(product.productCategory, 3)
+
+    def testBooks(self):
+        c = Client()
+        productList = Products.objects.filter(productCategory='4')
+        response = c.post('/books/1/0/')
+        self.assertEqual(response.status_code, 200)
+        for product in productList:
+            self.assertEqual(product.productCategory, 4)
 
 
 if __name__ == '__main__':
